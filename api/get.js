9db2cc30e6f05e6d41cb9825425ae69a1d89b6e7
Varshith302006 <varshirohith9@gmail.com>
2025-10-29 20:02:55 +0530

    Update get.js

--- api/get.js
+++ api/get.js
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 export default async function handler(req, res) {
+  try{
   if (req.method !== "POST") return res.status(405).json({ error: "Method Not Allowed" });
   const { username, password } = req.body;
   const { data, error } = await supabase
@@ -16,4 +17,9 @@ export default async function handler(req, res) {
   if (!data) return res.status(404).json({ error: "Not found" });
 
   return res.json(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: e.message || "Server error" });
+  }
+
 }
